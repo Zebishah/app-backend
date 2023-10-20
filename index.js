37879c28cacab802236727317de91dde4f01163a
Zohaib haider <zebihaider123@gmail.com>
2023-10-20 19:26:46 +0500

    ch

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ app.use(cors({
     credentials: true
 }));
 app.use(express.json())
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 connectDB();
 let host = process.env.REACT_APP_API_HOST
 app.get('/', (req, res) => {
     res.json({ message: 'API is up and running' });
 });
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/notes', require('./routes/notes'))
+// app.use('/api/auth', require('./routes/auth'))
+// app.use('/api/notes', require('./routes/notes'))
 app.listen(port, () => {
     console.log("Server is Listening at Port" + host);
 })
