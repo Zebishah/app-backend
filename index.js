69ae4a61767c46560bb6919749a611d9c3f7c87c
Zohaib haider <zebihaider123@gmail.com>
2023-10-20 19:34:28 +0500

    vv

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ let host = process.env.REACT_APP_API_HOST
 app.get('/', (req, res) => {
     res.json({ message: 'API is up and running' });
 });
-// app.use('/api/auth', require('./routes/auth'))
-app.use('/api/notes', require('./routes/notes'))
+app.use('/api/auth', require('./routes/auth'))
+// app.use('/api/notes', require('./routes/notes'))
 app.listen(port, () => {
     console.log("Server is Listening at Port" + host);
 })
