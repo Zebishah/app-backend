61abf887259e2b2017267a76130ad0dd23360a29
Zohaib haider <zebihaider123@gmail.com>
2023-10-20 18:22:31 +0500

    changes

--- index.js
+++ index.js
@@ -5,14 +5,17 @@ const app = express();
 require('dotenv').config();
 var cors = require('cors')
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3000", // Replace with your actual frontend domain
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
-}))
+}));
 app.use(express.json())
 const port = 5000;
 connectDB();
 let host = process.env.REACT_APP_API_HOST
+app.get('/', (req, res) => {
+    res.json({ message: 'API is up and running' });
+});
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 app.listen(port, () => {
