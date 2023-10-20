d1350ec57c2b92e790206f32a938667ef176a561
Zohaib haider <zebihaider123@gmail.com>
2023-10-20 22:11:02 +0500

    changes

--- routes/Notes.js
+++ routes/Notes.js
@@ -8,133 +8,136 @@ const router = express.Router();
 router.get('/fetchNote', fetchUser, async (req, res) => {
      try {
           let userId = req.user.id;
+          console.log(userId)
           let fetched_Notes = await notes.find({ user: userId });
+          console.log(fetched_Notes)
           res.send(fetched_Notes);
+
      } catch (error) {
           console.log("Error occurred in fetching notes: " + error);
           res.status(500).send("An issue occurred while fetching notes.");
      }
 })
 
-router.post('/addNote', fetchUser, [
-     body("title", "Title must not be that much smaller").isLength({ max: 20 }),
-     body("Description", "Description must not be that much smaller").isLength({ max: 256 }),
-], async (req, res) => {
-     try {
-          const result = validationResult(req);
+// router.post('/addNote', fetchUser, [
+//      body("title", "Title must not be that much smaller").isLength({ max: 20 }),
+//      body("Description", "Description must not be that much smaller").isLength({ max: 256 }),
+// ], async (req, res) => {
+//      try {
+//           const result = validationResult(req);
 
-          if (!result.isEmpty()) {
-               return res.status(400).json({ errors: result.array() });
-          } else {
+//           if (!result.isEmpty()) {
+//                return res.status(400).json({ errors: result.array() });
+//           } else {
 
-               userId = await users.id;
-               console.log(userId)
-               let { title, Description, tags } = req.body;
+//                userId = await users.id;
+//                console.log(userId)
+//                let { title, Description, tags } = req.body;
 
-               let note = await new notes({
-                    title, Description, tags, user: userId
-               })
-               let savedNote = await note.save();
-               res.json(savedNote);
-          }
-
-
-     } catch (error) {
-          console.log("Error occurred in Adding notes: " + error);
-          res.status(500).send("An issue occurred while adding notes.");
-     }
-})
-
-router.put('/updateNote/:id', fetchUser, [
-     body("title", "Title must not be that much smaller").isLength({ max: 20 }),
-     body("Description", "Description must not be that much smaller").isLength({ max: 256 }),], async (req, res) => {
-          try {
-
-               const result = validationResult(req);
-
-               if (!result.isEmpty()) {
-                    res.status(400).json({ errors: result.array() });
-               } else {
-                    userId = await users.id;
-                    let { title, Description, tags } = req.body;
-                    let newNote = {};
-                    if (title) {
-                         newNote.title = title;
-
-                    }
-                    if (Description) {
-                         newNote.Description = Description;
-                    }
-                    if (tags) {
-                         newNote.tags = tags;
-                    }
-                    let note = await notes.findById(req.params.id);
-                    console.log(note)
-                    if (!note) {
-                         res.status(404).send("note with that id not found.....");
-                    }
-                    else {
-                         console.log(note.user.toString())
-                         if (note.user.toString() !== userId) {
-                              res.status(404).send("Not Allowed........");
-                         }
-                         else {
-                              let updated_Note = await notes.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
-                              res.json(updated_Note);
-
-                         }
-                    }
+//                let note = await new notes({
+//                     title, Description, tags, user: userId
+//                })
+//                let savedNote = await note.save();
+//                res.json(savedNote);
+//           }
 
 
-               }
+//      } catch (error) {
+//           console.log("Error occurred in Adding notes: " + error);
+//           res.status(500).send("An issue occurred while adding notes.");
+//      }
+// })
+
+// router.put('/updateNote/:id', fetchUser, [
+//      body("title", "Title must not be that much smaller").isLength({ max: 20 }),
+//      body("Description", "Description must not be that much smaller").isLength({ max: 256 }),], async (req, res) => {
+//           try {
+
+//                const result = validationResult(req);
+
+//                if (!result.isEmpty()) {
+//                     res.status(400).json({ errors: result.array() });
+//                } else {
+//                     userId = await users.id;
+//                     let { title, Description, tags } = req.body;
+//                     let newNote = {};
+//                     if (title) {
+//                          newNote.title = title;
+
+//                     }
+//                     if (Description) {
+//                          newNote.Description = Description;
+//                     }
+//                     if (tags) {
+//                          newNote.tags = tags;
+//                     }
+//                     let note = await notes.findById(req.params.id);
+//                     console.log(note)
+//                     if (!note) {
+//                          res.status(404).send("note with that id not found.....");
+//                     }
+//                     else {
+//                          console.log(note.user.toString())
+//                          if (note.user.toString() !== userId) {
+//                               res.status(404).send("Not Allowed........");
+//                          }
+//                          else {
+//                               let updated_Note = await notes.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
+//                               res.json(updated_Note);
+
+//                          }
+//                     }
 
 
-          } catch (error) {
-               console.log("Here is error occured in updating notes" + error);
-
-               res.status(500).send("Code have some issues in updating notes part.....");
-
-          }
-     })
-router.delete('/deleteNote/:id', fetchUser, async (req, res) => {
-     try {
-
-          const result = validationResult(req);
-
-          if (!result.isEmpty()) {
-               res.status(400).json({ errors: result.array() });
-          } else {
-               userId = await users.id;
+//                }
 
 
-               let note = await notes.findById(req.params.id);
-               console.log(note)
-               if (!note) {
-                    res.status(404).send("note with that id not found.....");
-               }
-               else {
+//           } catch (error) {
+//                console.log("Here is error occured in updating notes" + error);
 
-                    if (note.user.toString() !== userId) {
-                         res.status(404).send("Not Allowed........");
-                    }
-                    else {
-                         let deleted_Note = await notes.findByIdAndDelete(req.params.id);
-                         res.json(deleted_Note);
+//                res.status(500).send("Code have some issues in updating notes part.....");
 
-                    }
-               }
+//           }
+//      })
+// router.delete('/deleteNote/:id', fetchUser, async (req, res) => {
+//      try {
 
+//           const result = validationResult(req);
 
-          }
+//           if (!result.isEmpty()) {
+//                res.status(400).json({ errors: result.array() });
+//           } else {
+//                userId = await users.id;
 
 
-     } catch (error) {
-          console.log("Here is error occured in updating notes" + error);
+//                let note = await notes.findById(req.params.id);
+//                console.log(note)
+//                if (!note) {
+//                     res.status(404).send("note with that id not found.....");
+//                }
+//                else {
 
-          res.status(500).send("Code have some issues in updating notes part.....");
+//                     if (note.user.toString() !== userId) {
+//                          res.status(404).send("Not Allowed........");
+//                     }
+//                     else {
+//                          let deleted_Note = await notes.findByIdAndDelete(req.params.id);
+//                          res.json(deleted_Note);
 
-     }
-})
+//                     }
+//                }
+
+
+//           }
+
+
+//      } catch (error) {
+//           console.log("Here is error occured in updating notes" + error);
+
+//           res.status(500).send("Code have some issues in updating notes part.....");
+
+//      }
+// })
 
 
 module.exports = router;
